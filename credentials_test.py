71cc3d823cf044b6a08b684fde57b679da85ef31
Libb521 <eotieno39@yahoo.com>
2020-01-26 15:54:23 +0300

    carried out a third test and passed, this test is to test if we can save multiple credentials

--- credentials_test.py
+++ credentials_test.py
@@ -33,6 +33,22 @@ class TestCredentials(unittest.TestCase):
         self.new_credentials.save_credentials()
         self.assertEqual(len(Credentials.credentials_list),1)
 
+    def tearDown(self):
+
+
+            Credentials.credentials_list = []
+   
+
+    def test_save_multiple_credentials(self):
+        '''
+        test_save_multiple_contact to check if we can save multiple contact
+        objects to our contact_list
+        '''
+        self.new_credentials.save_credentials()
+        test_save_credentials = Credentials("Test","user","0712345678","test@user.com") # new contact
+        test_save_credentials.save_credentials()
+        self.assertEqual(len(Credentials.credentials_list),2)
+
 
 if __name__ == '__main__':
     unittest.main()

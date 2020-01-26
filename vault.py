d1eaa918963893720697b708e870d703dcfa2a15
Libb521 <eotieno39@yahoo.com>
2020-01-26 12:40:16 +0300

    created four instances that we need to take up our dets i.e name, dets,password and email

--- vault.py
+++ vault.py
@@ -1,8 +1,18 @@
 import pyperclip
 import random
+
 class Dets:
-    """
+    '''
     Class Dets to create new Detail credentials
-    """
+    '''
+
+    dets_list = []
+
+    def _init_(self,name,dets,password,email):
+
+        self.name = name
+        self.dets = dets
+        self.password = password
+        self.email = email
 
     

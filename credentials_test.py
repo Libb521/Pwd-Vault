e69ce32e4521a949fbabecafbbcd605e39595cca
Libb521 <eotieno39@yahoo.com>
2020-01-26 14:13:08 +0300

    initiated the first testand instructed that a new credential is created before the next test

--- credentials_test.py
+++ credentials_test.py
@@ -1,2 +1,30 @@
 import unittest
+import pyperclip
 from credentials import Credentials
+
+class TestCredentials(unittest.TestCase):
+
+    '''
+    This test class defines test cases for the credentials behavior
+    '''
+
+    def setUp(self):
+        '''
+        This is to run before each test case.
+        '''
+        self.new_credentials = Credentials("Libby","instagram","Libby19","eotieno39@yahoo.com")
+
+    def test_init(self):
+        '''
+        This is to test if it has been initialized properly
+        '''
+
+        self.assertEqual(self.new_credentials.name,"Libby")
+        self.assertEqual(self.new_credentials.credentials,"instagram")
+        self.assertEqual(self.new_credentials.password,"Libby19")
+        self.assertEqual(self.new_credentials.email,"eotieno39@yahoo.com")
+
+if __name__ == '__main__':
+    unittest.main()
+
+

dee3ae8f9ab36c1407fb516f036a97e2cdc1e074
Libb521 <eotieno39@yahoo.com>
2020-01-27 16:33:39 +0300

    created a test file to test the code in the user file

--- user.py
+++ user.py
@@ -2,6 +2,27 @@ import pyperclip
 
 class User:
     """
-    Class that generates new instances of contacts.
+    This is to generate new contacts.
     """
+    user_list =[]
+
+    def __init__(self,name1,name2,password,email):
+
+         self.name1 = name1
+         self.name2 = name2
+         self.password = password
+         self.email = email
+
+    def save_user(self):
+        User.user_list.append(self)
+
+    def delete_user(self):
+
+        '''
+        This is to delete user from list
+        '''
+
+        User.user_list.remove(self)
+
+    
 
